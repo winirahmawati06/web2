4ccba4199cab09ec61729deb68e6331bf7c63977
winirahmawati06 <winirachmawati06@gmail.com>
2017-02-06 20:58:36 +0700

    Add files via upload

---
+++ tugas/tugas 2/index.php
@@ -0,0 +1,71 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<title>Operasi CRUD</title>
+<style>
+div.container {
+	width:100px;
+	border: 1px solid gray;
+}
+header, footer {
+	padding: 1em;
+	color:#FFFFFF;
+	background-color:#663300;
+	clear:left;
+	text-align :center;
+}
+nav {
+	float:left; 
+	max-width: 160px;
+	margin:0;
+	padding:1em;
+}
+nav ul {
+	list-style-type:none;
+	padding:0;
+}
+nav ul a{
+	text-decoration:none;
+}
+artitle {
+	margin-left:170px;
+	border-left:1px solid gray;
+	padding:1em;
+	overflow:hidden;
+}
+</style>
+</head>
+<body>
+<div class="Container">
+<header>
+	<h1>Operasi CRUD</h1>
+</header>
+
+<nav>
+	<ul>
+		<li><b>MENU</b></li>
+		<li><a href="#">Menu 1</a></li>
+		<li><a href="#">Menu 2</a></li>
+		<li><a href="#">Menu 3</a></li>
+	</ul>
+</nav>>
+<article>
+<table border="1" width="75%" align="center">
+<tr>
+<th><a href="tambah.php"><button type="submit">+ tambah</button></a>
+</th>
+</tr>
+<tr>
+	<td>No</td>
+	<td>Nama</td>
+	<td>Nim</td>
+	<td>Alamat</td>
+	<td>Aksi</td>
+</tr>
+</table>
+</article>
+<footer>Winirahmawati-14111103</footer>
+</div>
+</body>
+</html>
